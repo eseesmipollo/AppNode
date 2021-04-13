2271a1fc327d88e67166038fdc9b84d91ee12e80
LautaroMartinez <lautaro.martinez@southworks.com>
2021-04-13 16:55:12 -0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const uri = process.env.npm_package_config_uri ;
 
+//connect mongoDB
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('Database connected'))
+.catch(e => console.log(e))
+
+//routes
+app.use('/users', require('./router/Users'));
+
+//
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
